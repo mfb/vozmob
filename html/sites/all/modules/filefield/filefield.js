9a4b265214e9a9fee4d37080745574a0427cebb6
mark <mark@indymedia.org>
2008-12-08 01:35:51 -0800

    update various cck & field modules

--- html/sites/all/modules/filefield/filefield.js
+++ html/sites/all/modules/filefield/filefield.js
@@ -1,24 +1,25 @@
-// $Id: filefield.js,v 1.10 2008/11/26 00:49:33 drewish Exp $
+// $Id: filefield.js,v 1.12 2008/12/03 21:50:52 drewish Exp $
 
 /**
  * Auto-attach standard client side file input validation.
  */
 Drupal.behaviors.filefieldValidateAutoAttach = function(context) {
   $("input[@type='file'][accept]", context).change( function() {
-    $('.filefield-js-error').remove();
+    // Remove any previous errors.
+    $('.file-upload-js-error').remove();
 
     /**
      * Add client side validation for the input[@file] accept attribute.
      */
-    if(this.accept.length > 1){
-      accept = this.accept.replace(/,/g, '|');
-      v = new RegExp('\\.(' + (accept ? accept : '') + ')$', 'gi');
+    var accept = this.accept.replace(/,\s*/g, '|');
+    if (accept.length > 1) {
+      var v = new RegExp('\\.(' + accept + ')$', 'gi');
       if (!v.test(this.value)) {
         var error = Drupal.t("The selected file %filename cannot not be uploaded. Only files with the following extensions are allowed: %extensions.",
-          { '%filename' : this.value, '%extensions': accept.replace(/\|/g, ', ') }
+          { '%filename' : this.value, '%extensions' : accept.replace(/\|/g, ', ') }
         );
-        // what do I prepend this to?
-        $(this).before('<div class="messages error filefield-js-error">' + error + '</div>');
+        // What do I prepend this to?
+        $(this).before('<div class="messages error file-upload-js-error">' + error + '</div>');
         this.value = '';
         return false;
       }
