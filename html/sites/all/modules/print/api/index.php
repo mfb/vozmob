b69d1dbb2300521a723633deeb57d8ef0e4623ed
mfb <mfburdett@gmail.com>
2009-03-02 13:39:07 -0800

    replace printpdf module with print module and tcpdf

---
+++ html/sites/all/modules/print/api/index.php
@@ -0,0 +1,31 @@
+<?php
+// $Id: index.php,v 1.2.2.5 2008/09/25 22:11:55 jcnventura Exp $
+
+/**
+ * @file
+ * Homepage of the API reference
+ */
+
+/** @mainpage Printer, e-mail and PDF versions
+ * Welcome to the module developer's documentation. The interesting functions for
+ * themers are those that start with 'theme_'.
+ *
+ * - Printer-friendly pages (core)
+ *   - @link print.module Module main file @endlink
+ *   - @link print.admin.inc Settings form @endlink
+ *   - @link print.pages.inc HTML generation @endlink
+ *   - @link print.install (Un)Install routines @endlink
+ *   - @link print.tpl.php Page generation template @endlink
+ * - Send by e-mail
+ *   - @link print_mail.module Module main file @endlink
+ *   - @link print_mail.admin.inc Settings form @endlink
+ *   - @link print_mail.inc Mail form and send mail routine @endlink
+ *   - @link print_mail.install (Un)Install routines @endlink
+ * - PDF version
+ *   - @link print_pdf.module Module main file @endlink
+ *   - @link print_pdf.admin.inc Settings form @endlink
+ *   - @link print_pdf.pages.inc PDF generation @endlink
+ *   - @link print_pdf.class.inc Auxiliary PHP5 class @endlink
+ *   - @link print_pdf.class_php4.inc Auxiliary PHP4 class @endlink
+ *   - @link print_pdf.install (Un)Install routines @endlink
+ */
