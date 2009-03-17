9509cd039b00241bc21e376cfb6eab8e38006cd4
mfb <mfburdett@gmail.com>
2009-03-17 15:36:36 -0700

    fix imagefield module and preliminary work on overlay and MMS messaging plugin

--- html/sites/all/themes/blueprint/scripts/cycle.js
+++ html/sites/all/themes/blueprint/scripts/cycle.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   // Set up the slideshow.
   var cycleIndex = 0;
-  $('div.field-type-image div.field-items').each(function() {
+  $('div.field-field-image div.field-items').each(function() {
     $(this).before('<div class="cycle-nav" id="cycle-nav-' + cycleIndex + '"></div>').cycle({ 
       fx:    'fade',
       pager: '#cycle-nav-' + cycleIndex,
@@ -12,8 +12,19 @@ $(document).ready(function() {
     cycleIndex = 1 + cycleIndex;
   });
   // Set the height of the field since its images are now absolutely positioned.
-  $('div.field-type-image div.field-items').each(function() {
+  $('div.field-field-image div.field-items').each(function() {
     // This won't work so well for a set of images with wildly different heights.
     $(this).height($(this).find('img').outerHeight());
+  });
+  // Set up the overlay and expose behaviors.
+  $('div.field-field-image div.field-items').each(function() {
+    $(this).overlay({
+      onBeforeLoad: function() {
+        this.getBackgroundImage().expose({color: '#000'});
+      },
+      onClose: function() {
+        $.expose.close();
+      }
+    });
   });
 });
