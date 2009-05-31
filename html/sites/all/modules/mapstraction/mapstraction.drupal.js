212aa8c7281273a7b4048c3a26b1b3caffcd263e
mfb <mark@subtle.(none)>
2009-05-30 23:58:59 -0700

    switch to 6--1 branch of mapstraction

--- html/sites/all/modules/mapstraction/mapstraction.drupal.js
+++ html/sites/all/modules/mapstraction/mapstraction.drupal.js
@@ -1,21 +1,29 @@
+// $Id: mapstraction.drupal.js,v 1.1.2.7 2009/05/06 15:04:14 dixon Exp $
 $(document).ready(function() {
   Drupal.mapstraction = [];
   $(Drupal.settings.mapstraction).each(function(index) {
     var id = 'mapstraction-' + this.viewName + '-' + this.currentDisplay;
     Drupal.mapstraction[id] = new Mapstraction(id, this.apiName);
-    
-    // Set start point
-    var startPoint = new LatLonPoint(38.92, -77.04);
-    Drupal.mapstraction[id].setCenterAndZoom(startPoint, 13);
-    
-    // Set up controls
-    Drupal.mapstraction[id].addControls(this.controls);
-    
-    // Set up markers & info bubbles
+
+    // Set up markers and info bubbles.                                                                                                                                                                                                                      
     $(this.markers).each(function(index) {
-      marker = new Marker(new LatLonPoint(this.lat, this.lon));
+      var markerPoint = new LatLonPoint(Number(this.lat), Number(this.lon));
+      marker = new Marker(markerPoint);
       marker.setInfoBubble(this.title);
+      marker.setIcon(this.icon);
       Drupal.mapstraction[id].addMarker(marker);
     });
+
+    // Set up controls.                                                                                                                                                                                                                     
+    Drupal.mapstraction[id].addControls(this.controls);
+
+    // Set auto center and zoom.                                                                                                                                                                                                            
+    if (this.initialPoint.auto) {
+      Drupal.mapstraction[id].autoCenterAndZoom();
+    }
+    else {
+      var startPoint = new LatLonPoint(Number(this.initialPoint.latitude), Number(this.initialPoint.longitude));
+      Drupal.mapstraction[id].setCenterAndZoom(startPoint, Number(this.initialPoint.zoom));
+    }
   });
 });
