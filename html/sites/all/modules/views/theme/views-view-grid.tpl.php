8b653b8f0e6faafd251030f22082ec1dcb8abd2a
mark burdett <mfburdett@gmail.com>
2011-02-04 14:43:40 -0800

    merge Views bugfixes from cvs.drupal.org

--- html/sites/all/modules/views/theme/views-view-grid.tpl.php
+++ html/sites/all/modules/views/theme/views-view-grid.tpl.php
@@ -1,19 +1,20 @@
 <?php
-// $Id: views-view-grid.tpl.php,v 1.3.2.3 2010/12/08 23:17:31 merlinofchaos Exp $
+// $Id: views-view-grid.tpl.php,v 1.3.2.4 2011/01/20 20:07:08 merlinofchaos Exp $
 /**
  * @file views-view-grid.tpl.php
  * Default simple view template to display a rows in a grid.
  *
  * - $rows contains a nested array of rows. Each row contains an array of
  *   columns.
- *
+ * - $class contains the class of the table.
+ * - $attributes contains other attributes for the table.
  * @ingroup views_templates
  */
 ?>
 <?php if (!empty($title)) : ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<table class="views-view-grid"<?php print $attributes; ?>>
+<table class="<?php print $class; ?>"<?php print $attributes; ?>>
   <tbody>
     <?php foreach ($rows as $row_number => $columns): ?>
       <tr class="<?php print $row_classes[$row_number]; ?>">
